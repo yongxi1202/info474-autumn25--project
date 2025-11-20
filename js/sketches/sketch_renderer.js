70b7408f8eecafc46b040f5095998d2d7e0e4cdc
sukjinchung <sukjin@uw.edu>
2025-11-20 11:06:32 -0800

    update story

--- js/sketches/sketch_renderer.js
+++ js/sketches/sketch_renderer.js
@@ -20,27 +20,18 @@
 
         draw: function (p, manager, ai, progress) {
             try { console.log('Renderer: delegating draw, ai=', ai); } catch (e) { }
+            p.clear();
 
-            if (ai === 0 || ai === 1) {
-                window.VizTitle.draw(p, manager, ai, progress);
+            if (ai === 1 || ai === 2) {
+                window.VizTravel.draw(p, manager, ai, progress);
                 return;
             }
 
-            if (ai >= 2 && ai <= 4) {
-                window.VizTravel.draw(p, manager, ai - 2, progress);
-                return;
-            }
-
-
-            if (ai >= 5 && ai < 7) {
+            if (ai === 3) {
                 window.Origins.draw(p, manager, ai, progress);
                 return;
             }
-
-            if (ai === 7) {
-                window.VizBar.draw(p, manager, ai, progress);
-                return;
-            }
+            
         }
     };
 })();
