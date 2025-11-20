1d80dd8de88985addab75ff08a227f96b61a7b81
sukjinchung <sukjin@uw.edu>
2025-11-20 12:10:46 -0800

    update rendered

--- js/sketches/sketch_renderer.js
+++ js/sketches/sketch_renderer.js
@@ -20,22 +20,13 @@
 
         draw: function (p, manager, ai, progress) {
             try { console.log('Renderer: delegating draw, ai=', ai); } catch (e) { }
+            p.clear();
 
-            if (ai === 0 || ai === 1) {
-                window.VizTitle.draw(p, manager, ai, progress);
+            if (ai === 1 || ai === 2) {
+                window.VizTravel.draw(p, manager, ai, progress);
                 return;
             }
 
-<<<<<<< HEAD
-            if (ai >= 2 && ai <= 4) {
-                window.VizTravel.draw(p, manager, ai - 2, progress);
-                return;
-            }
-
-
-            if (ai >= 5 && ai < 7) {
-                window.Origins.draw(p, manager, ai, progress);
-=======
             if (ai === 3) {
                 window.VizOrigins.draw(p, manager, ai, progress);
                 return;
@@ -43,19 +34,9 @@
 
             if (ai >= 4) {
                 window.VizAirlines.draw(p, manager, ai, progress);
->>>>>>> cf761a2 (added most popular airline)
                 return;
             }
-
-            if (ai === 7 || ai === 8) {
-                window.VizC.draw(p, manager, ai, progress);
-                return;
-            }
-
-            if (ai === 9) {
-                window.VizBar.draw(p, manager, ai, progress);
-                return;
-            }
+            
         }
     };
 })();
